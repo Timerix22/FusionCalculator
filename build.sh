8a5370c09e6aaf0fdb32163ebe25a97dbe424934
Timerix22 <timerix11@gmail.com>
2023-12-19 19:08:19 +0600

    fixed memory and visibility problems

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ set -eo pipefail
 rm -rf out bin
 mkdir out bin
 fut -l c -o out/FusionCalculator.c src/*.fu
-gcc -Wall -O2 out/FusionCalculator.c -o bin/FusionCalculator.exe $(pkg-config --cflags glib-2.0) $(pkg-config --libs glib-2.0)
+gcc -Wall -Wno-unused-value -Wno-unused-function -O0 -g out/FusionCalculator.c -o bin/FusionCalculator.exe $(pkg-config --cflags glib-2.0) $(pkg-config --libs glib-2.0)
