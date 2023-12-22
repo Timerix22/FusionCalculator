cc6cda554c993187ac04ea7329a90ef6fb59ca6a
Timerix22 <timerix11@gmail.com>
2023-12-22 20:17:56 +0600

    Parser.BuildExpressionTree

--- build.sh
+++ build.sh
@@ -3,14 +3,15 @@ set -eo pipefail
 
 OUT_FILE="out/FusionCalculator.c"
 BIN_FILE="bin/FusionCalculator.exe"
+SRC_FILES="$(find src/ -name '*.fu')"
 WARNINGS="-Wall -Wno-unused-value -Wno-unused-function -Wno-unused-variable -Wno-discarded-qualifiers"
 INCLUDES="$(pkg-config --cflags glib-2.0)"
 COMPILER_ARGS="-O0 -g"
-LINKER_ARGS="$(pkg-config --libs glib-2.0)"
+LINKER_ARGS="$(pkg-config --libs glib-2.0) -lm"
 
 rm -rf out bin
 mkdir out bin
 
-fut -l c -o "$OUT_FILE" src/*.fu
+fut -l c -D C -o "$OUT_FILE" $SRC_FILES
 
 gcc $WARNINGS $COMPILER_ARGS "$OUT_FILE" -o "$BIN_FILE" $INCLUDES $LINKER_ARGS
